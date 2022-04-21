0be69a3d4b631d48828c8199c8fc621ac2d81b7b
chidin194 <chiara.dinan@softwire.com>
2022-04-21 17:33:17 +0100

    SupportBank: Added classes and program now accepts user input

--- classes.js
+++ classes.js
@@ -1,16 +1,26 @@
 class Account {
-    constructor(accountName, balance) {
+    constructor(accountName, transactionList) {
         this.accountName = accountName;
-        this.balance = balance;
+        this.transactionList = transactionList;
     }
 }
 
 class Transaction {
-    constructor(date, accountFrom, accountTo, amount, narrative) {
+    constructor(date, accountFrom, accountTo, narrative, amount) {
         this.date = date;
         this.accountFrom = accountFrom;
         this.accountTo = accountTo;
-        this.amount = amount;
         this.narrative = narrative;
+        this.amount = amount;
     }
 }
+
+class Bank {
+    constructor(accountList) {
+        this.accountList = accountList;
+    }
+}
+
+// export { Account, Transaction, Bank }
+
+module.exports = { Account, Transaction, Bank }
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+
+const {Bank} = require('./classes.js')
+const {Transaction} = require('./classes.js')
+const {Account} = require('./classes.js')
 const csv = require('csv-parser');
 const fs = require('fs');
+const ps = require('prompt-sync');
+const prompt = ps();
 
-// let accountList = [];
+
+
+
 let transactions2014Json = [];
 
 function csvToJson() {
@@ -26,19 +34,63 @@ const getTransactions = async() => {
 }
 
 const runProgram = async () => {
-    const result = await getTransactions();
-    return result
+
+    let userCommand = prompt("Please enter a command:")
+
+    const transactions = await getTransactions();
+    let newBank = new Bank([])
+
+    transactions.forEach(transaction => {
+        for (let key in transaction) {
+            if (key === "From" || key == "To") {
+                if (newBank.accountList.indexOf(transaction[key]) < 0) {
+                    newBank.accountList.push(transaction[key])
+                }
+            }
+        }
+    });
+
+    let transactionList = [];
+    transactions.forEach(transaction => {
+        let newTransaction = new Transaction(
+            transaction.Date,
+            transaction.From,
+            transaction.To,
+            transaction.Narrative,
+            transaction.Amount
+        )
+        transactionList.push(newTransaction);
+    })
+
+
+        if (userCommand === "List All") {
+            console.log(newBank.accountList.toString());
+        } else if (userCommand ) {
+            let userTransactionList = []
+            transactionList.forEach(transaction => {
+                if (transaction.accountFrom === userCommand.substring(5)|| transaction.accountTo === userCommand.substring(5)) {
+                    userTransactionList.push(transaction)
+
+
+
+                }
+            })
+
+            console.log(`
+                        Total transactions found: ${userTransactionList.length}`)
+
+            for (const t of userTransactionList)
+            {
+                console.log(`
+                        ${t.date}: ${t.accountFrom} paid £${t.amount} to ${t.accountTo}
+                        Ref: ${t.narrative}`);
+            }
+        }
+
 }
 
-const transactions = runProgram();
 
-transactions.forEach(transaction => {
-    for (let key in transaction) {
-        console.log(`${key}: ${transaction[key]}`);
-    }
-});
-
-
+runProgram();
 
 
 
